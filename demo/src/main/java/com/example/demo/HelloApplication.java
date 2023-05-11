e68f229e544defe8a93ab78ec6fb165b98b1aed7
alinaduca <alina_duca@yahoo.com>
2023-05-11 17:14:04 +0300

    Informatii despre student

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -7,21 +7,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -31,16 +25,23 @@ import java.util.stream.Collectors;
 public class HelloApplication extends Application {
     private String firstName;
     private String lastName;
-    private double textFieldWidth = 200;
     private String nrMatricol;
     private String email;
     private String telefon;
-    private boolean gen;
+    private String gen;
     private String facultate;
+    private String mesaj;
     private double medie;
-    private List<String> preferinte;
+    Camin camin1;
+    Camin camin2;
+    Camin camin3;
+    Camin camin4;
+    Camin camin5;
+    private List<Camin> preferinteCamine = new ArrayList<>();
+
     @Override
     public void start(Stage stage) throws SQLException {
+        final double textFieldWidth = 200;
         Connection connection = DatabaseConnection.getInstance().getConnection();
         Accomodation accomodation = new Accomodation(connection);
         accomodation.RepartizareStudentiInCamin();
@@ -83,7 +84,6 @@ public class HelloApplication extends Application {
         facultateComboBox.setPrefWidth(textFieldWidth);
         facultateComboBox.setMaxWidth(textFieldWidth);
         HBox facultatePanel = new HBox(facultateLabel, facultateComboBox);
-
 
         Label medieLabel = new Label("Media pe ultimul an universitar încheiat: ");
         TextField medieTextField = new TextField();
@@ -142,8 +142,8 @@ public class HelloApplication extends Application {
                 listCamine3.clear();
                 listCamine4.clear();
                 listCamine1.addAll(listCamine);
-                listCamine1.remove(Camin.getByName(t1));
-                System.out.println(Camin.getByName(t1));
+                camin1 = Camin.getByName(t1);
+                listCamine1.remove(camin1);
                 optionsCamine1.setAll(listCamine1.stream().map(camin -> camin.getNume()).collect(Collectors.toList()));
                 optionsCamine2.clear();
                 optionsCamine3.clear();
@@ -162,10 +162,11 @@ public class HelloApplication extends Application {
                 listCamine2.clear();
                 listCamine3.clear();
                 listCamine4.clear();
-                listCamine2.addAll(listCamine);
-                listCamine2.remove(Camin.getByName(t1));
-                System.out.println(Camin.getByName(t1));
-                optionsCamine2.setAll(listCamine1.stream().map(camin -> camin.getNume()).collect(Collectors.toList()));
+                listCamine2.addAll(listCamine1);
+                camin2 = Camin.getByName(t1);
+                listCamine2.remove(camin2);
+                listCamine2.remove(camin1);
+                optionsCamine2.setAll(listCamine2.stream().map(camin -> camin.getNume()).collect(Collectors.toList()));
                 optionsCamine3.clear();
                 optionsCamine4.clear();
             }
@@ -182,10 +183,12 @@ public class HelloApplication extends Application {
             public void changed(ObservableValue ov, String t, String t1) {
                 listCamine3.clear();
                 listCamine4.clear();
-                listCamine3.addAll(listCamine);
-                listCamine3.remove(Camin.getByName(t1));
-                System.out.println(Camin.getByName(t1));
-                optionsCamine3.setAll(listCamine1.stream().map(camin -> camin.getNume()).collect(Collectors.toList()));
+                listCamine3.addAll(listCamine2);
+                camin3 = Camin.getByName(t1);
+                listCamine3.remove(camin3);
+                listCamine3.remove(camin2);
+                listCamine3.remove(camin1);
+                optionsCamine3.setAll(listCamine3.stream().map(camin -> camin.getNume()).collect(Collectors.toList()));
                 optionsCamine4.clear();
             }
         });
@@ -201,15 +204,21 @@ public class HelloApplication extends Application {
             public void changed(ObservableValue ov, String t, String t1) {
                 listCamine4.clear();
                 listCamine3.addAll(listCamine);
-                listCamine4.remove(Camin.getByName(t1));
-                System.out.println(Camin.getByName(t1));
-                optionsCamine4.setAll(listCamine1.stream().map(camin -> camin.getNume()).collect(Collectors.toList()));
+                camin4 = Camin.getByName(t1);
+                listCamine4.remove(camin1);
+                listCamine4.remove(camin2);
+                listCamine4.remove(camin3);
+                listCamine4.remove(camin4);
+                optionsCamine4.setAll(listCamine4.stream().map(camin -> camin.getNume()).collect(Collectors.toList()));
             }
         });
 
-
-
-
+        camineComboBox4.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue ov, String t, String t1) {
+                camin5 = Camin.getByName(t1);
+            }
+        });
 
         Label pref2 = new Label(" 2.) ");
         HBox pref2Panel = new HBox(pref2, camineComboBox1);
@@ -228,54 +237,136 @@ public class HelloApplication extends Application {
         HBox preferintePanel = new HBox(preferinteLabel, preferinte);
 
         Label genreLabel = new Label("Gen: ");
-        final ToggleGroup group = new ToggleGroup();
+        ToggleGroup group = new ToggleGroup();
         RadioButton rb1 = new RadioButton("Feminin");
         rb1.setToggleGroup(group);
         RadioButton rb2 = new RadioButton("Masculin");
         rb2.setToggleGroup(group);
-
+        group.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                if(((RadioButton)newValue).getText().equals("Feminin")) {
+                    gen = "Feminin";
+                }
+                else {
+                    gen = "Masculin";
+                }
+            }
+        });
         VBox bifat = new VBox(rb1, rb2);
         bifat.setPadding(new Insets(0, 0, 0, 10));
         bifat.setSpacing(5);
         HBox genrePanel = new HBox(genreLabel, bifat);
 
-        Canvas canvas = new Canvas(800, 600);
+        Canvas canvas = new Canvas(700, 600);
         VBox configPanel = new VBox(numePanel, prenumePanel,
                 nrMatricolPanel, emailPanel,
                 telefonPanel, facultatePanel,
                 mediePanel, preferintePanel, genrePanel
         );
-        configPanel.setPadding(new Insets(0, 0, 0, 40));
+        configPanel.setPadding(new Insets(0, 0, 0, 100));
         configPanel.setAlignment(Pos.CENTER);
         configPanel.setSpacing(10);
         Button submit = new Button("Aplicare");
+        Label mesajLabel = new Label();
+        VBox controlPanel = new VBox(mesajLabel, submit);
+        controlPanel.setSpacing(5);
         submit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                mesaj = null;
                 lastName = numeTextField.getText();
                 firstName = prenumeTextField.getText();
                 nrMatricol = nrMatricolTextField.getText();
                 email = emailTextField.getText();
                 telefon = telefonTextField.getText();
+                if(preferinteCamine != null) {
+                    preferinteCamine.clear();
+                }
+                if(camin1 != null) {
+                    preferinteCamine.add(camin1);
+                }
+                if(camin2 != null && !preferinteCamine.contains(camin2)) {
+                    preferinteCamine.add(camin2);
+                }
+                if(camin3 != null && !preferinteCamine.contains(camin3)) {
+                    preferinteCamine.add(camin3);
+                }
+                if(camin4 != null && !preferinteCamine.contains(camin4)) {
+                    preferinteCamine.add(camin4);
+                }
+                if(camin5 != null && !preferinteCamine.contains(camin5)) {
+                    preferinteCamine.add(camin5);
+                }
                 try {
                     medie = Double.parseDouble(medieTextField.getText());
                 } catch(NumberFormatException e) {
-                    System.out.println("Medie incorecta");
+                    mesaj = "Introduceți o medie validă.";
                 }
+                if(mesaj == null) {
+                    switch(valid(lastName, firstName, nrMatricol, email, telefon, facultate, medie, preferinteCamine, gen)) {
+                        case 0: mesaj = null; break;
+                        case 1: mesaj = "Introduceți numele."; break;
+                        case 2: mesaj = "Introduceți prenumele."; break;
+                        case 3: mesaj = "Introduceți numărul matricol."; break;
+                        case 4: mesaj = "Introduceți un email valid."; break;
+                        case 5: mesaj = "Introduceți un număr de telefon valid."; break;
+                        case 6: mesaj = "Selectați o facultate."; break;
+                        case 7: mesaj = "Introduceți o medie validă."; break;
+                        case 8: mesaj = "Selectați cel puțin un cămin."; break;
+                        case 9: mesaj = "Bifați genul."; break;
+                    }
+                }
+                mesajLabel.setText(mesaj);
             }
         });
-        HBox controlPanel = new HBox(submit);
         controlPanel.setAlignment(Pos.CENTER);
         root.setCenter(configPanel);
         root.setBottom(controlPanel);
-
-//        root.setCenter(canvas);
-        stage.setScene(new Scene(root, 800, 600));
+        stage.setScene(new Scene(root, 700, 600));
         stage.show();
     }
 
     public static void main(String[] args) throws SQLException {
         launch();
+    }
+
+    private int valid(String lastName, String firstName, String nrMatricol, String email, String telefon, String facultate, double medie, List<Camin> preferinte, String gen) {
+        if(lastName == null || lastName.length() < 2) {
+            return 1;
+        }
+        if(firstName == null || firstName.length() < 2) {
+            return 2;
+        }
+        if(nrMatricol == null || nrMatricol.length() < 2) {
+            return 3;
+        }
+        if(email == null || email.length() < 2 || !email.contains("@") || !email.contains(".")) {
+            return 4;
+        }
+        if(telefon == null || telefon.length() != 10) {
+            return 5;
+        }
+        for(int i = 0; i < telefon.length(); i++) {
+            char c = telefon.charAt(i);
+            String s = c + "";
+            if(!"0123456789".contains(s)) {
+                System.out.println(s + ",");
+                return 5;
+            }
+        }
+        if(facultate == null || facultate.length() < 2) {
+            return 6;
+        }
+        if(medie < 1 || medie > 10) {
+            return 7;
+        }
+        if(preferinte == null || preferinte.size() < 1) {
+            return 8;
+        }
+        if(gen == null || gen.length() < 2) {
+            return 9;
+        }
+        return 0;
     }
 }
 
